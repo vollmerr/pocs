640271aeeeea60c3487c6300f9a0ab6a8c0741c9
Vollmer <Ryan.Vollmer@dss.ca.gov>
2020-02-21 19:56:35 -0800

    use useEffect

--- intl/cra-react-intl/src/components/App/App.js
+++ intl/cra-react-intl/src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { IntlProvider } from 'react-intl';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -6,19 +6,21 @@ import en from '../../i18n/en';
 import Example from '../Example';
 import './App.css';
 
+const initialLocale = localStorage.getItem('locale') || 'en';
+
 const App = () => {
   const [messages, setMessages] = useState({ en, fr: null })
-  const [locale, setLocale] = useState('en');
-  
-  const changeLocale = (key) => {
-    if (!messages[key]) {
-      import(`../../i18n/${key}`).then((data) => {
-        setMessages({ ...messages, [key]: data });
+  const [locale, setLocale] = useState(initialLocale);
+
+  useEffect(() => {
+    if (!messages[locale]) {
+      import(`../../i18n/${locale}`).then((data) => {
+        setMessages({ ...messages, [locale]: data });
       });
     }
 
-    setLocale(key);
-  };
+    localStorage.setItem('locale', locale);
+  }, [locale, messages]);
 
   return (
     <Router>
@@ -28,8 +30,8 @@ const App = () => {
             in header...
           </header>
 
-          <button onClick={() => changeLocale('en')}>EN</button>
-          <button onClick={() => changeLocale('fr')}>FR</button>
+          <button onClick={() => setLocale('en')}>EN</button>
+          <button onClick={() => setLocale('fr')}>FR</button>
 
           <Switch>
             <Route path={'/'} component={Example} />
